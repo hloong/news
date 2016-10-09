6302ab39595f61866ee9ff50da336e2eb5978d6f
hloong <hbxglong@163.com>
2016-10-09 18:33:29 +0800

    修改main错误

--- app/src/main/java/com/hloong/news/MainActivity.java
+++ app/src/main/java/com/hloong/news/MainActivity.java
@@ -9,11 +9,15 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 
     @Override
-    protected void initVariables() {
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initPresenter() {
 
     }
 
